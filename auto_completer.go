989bd83f43af8ffd8c1d04334a3fb38515317e4f
lukasl-dev <git@lukasl.dev>
2022-08-20 01:18:27 +0200

    feat: add AutoCompleterFunc

--- auto_completer.go
+++ auto_completer.go
@@ -7,3 +7,11 @@ type AutoCompleter interface {
 	// the Context.
 	AutoComplete(ctx *Context) ([]*discordgo.ApplicationCommandOptionChoice, error)
 }
+
+// AutoCompleterFunc is a function that implements the AutoCompleter interface.
+type AutoCompleterFunc func(ctx *Context) ([]*discordgo.ApplicationCommandOptionChoice, error)
+
+// AutoComplete calls fn() itself and returns the result.
+func (fn AutoCompleterFunc) AutoComplete(ctx *Context) ([]*discordgo.ApplicationCommandOptionChoice, error) {
+	return fn(ctx)
+}
