4a44920d46670f59cd9db501cd6f4cc28e0c4c82
lukasl-dev <git@lukasl.dev>
2022-08-20 01:59:44 +0200

    refactor(auto-complete): change signature of AutoCompleter.AutoComplete()

--- auto_completer.go
+++ auto_completer.go
@@ -5,13 +5,13 @@ import "github.com/bwmarrin/discordgo"
 type AutoCompleter interface {
 	// AutoComplete returns an array of choices that match the current input of
 	// the Context.
-	AutoComplete(ctx *Context) ([]*discordgo.ApplicationCommandOptionChoice, error)
+	AutoComplete(ctx *Context) []*discordgo.ApplicationCommandOptionChoice
 }
 
 // AutoCompleterFunc is a function that implements the AutoCompleter interface.
-type AutoCompleterFunc func(ctx *Context) ([]*discordgo.ApplicationCommandOptionChoice, error)
+type AutoCompleterFunc func(ctx *Context) []*discordgo.ApplicationCommandOptionChoice
 
 // AutoComplete calls fn() itself and returns the result.
-func (fn AutoCompleterFunc) AutoComplete(ctx *Context) ([]*discordgo.ApplicationCommandOptionChoice, error) {
+func (fn AutoCompleterFunc) AutoComplete(ctx *Context) []*discordgo.ApplicationCommandOptionChoice {
 	return fn(ctx)
 }
