fe5ea76769f1f92d7edcfc5c12b82f30fb1aec73
lukasl-dev <git@lukasl.dev>
2022-08-20 00:45:15 +0200

    feat: add Context struct

---
+++ context.go
@@ -0,0 +1,25 @@
+package slashy
+
+import (
+	"github.com/bwmarrin/discordgo"
+	"strings"
+)
+
+type Context struct {
+	*discordgo.InteractionCreate
+}
+
+// Option searches for the first option that matches the given name and returns
+// it. If no option is found, nil is returned instead
+//
+// Option names are case-insensitive. Therefore, "option" and "OPTION" are
+// considered the same.
+func (ctx *Context) Option(name string) *discordgo.ApplicationCommandInteractionDataOption {
+	opts := ctx.ApplicationCommandData().Options
+	for _, opt := range opts {
+		if strings.EqualFold(opt.Name, name) {
+			return opt
+		}
+	}
+	return nil
+}
