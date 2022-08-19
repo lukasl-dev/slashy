9131153d700ea6fd46b58c535bc65a5357b199f3
lukasl-dev <git@lukasl.dev>
2022-08-20 01:17:59 +0200

    feat: add RunnerFunc

--- runner.go
+++ runner.go
@@ -1,10 +1,18 @@
 package slashy
 
 type Runner interface {
-	// Run executes the command with the given context. If the command fails,
+	// Run executes the Command with the given context. If the Command fails,
 	// the causing error is returned.
 	//
 	// The given Response is used to mutate the response message. The response
-	// message is built after the command is run successfully.
+	// message is built after the Command is run successfully.
 	Run(ctx *Context, resp *Response) error
+}
+
+// RunnerFunc is a function that implements the Runner interface.
+type RunnerFunc func(ctx *Context, resp *Response) error
+
+// Run calls fn() itself and returns the result.
+func (fn RunnerFunc) Run(ctx *Context, resp *Response) error {
+	return fn(ctx, resp)
 }
